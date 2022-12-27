32d6f82d4203a659cec1f7ae100726e303814fc0
HCY772304 <772304512@qq.com>
2022-12-27 16:26:12 +0800

    sv1.0.0

---
+++ IAR ARM - Default - EFR32MG21A010F1024IM32/sources.mk
@@ -0,0 +1,74 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+S79_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_UPPER_SRCS := 
+MSA_UPPER_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S79_UPPER_SRCS := 
+S_UPPER_SRCS := 
+MSA_SRCS := 
+O_SRCS := 
+ASM_UPPER_DEPS := 
+MSA_UPPER_DEPS := 
+EXECUTABLES := 
+OBJS := 
+ASM_DEPS := 
+S79_DEPS := 
+S_DEPS := 
+S_UPPER_DEPS := 
+MSA_DEPS := 
+C_DEPS := 
+S79_UPPER_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+00ikonke-app/driver \
+00ikonke-app/general \
+00ikonke-app \
+00ikonke-app/tools \
+. \
+address-table \
+antenna-stub \
+basic \
+ccm-encryption \
+coexistence-stub/EFR32 \
+counters \
+diagnostic-server \
+eeprom \
+efr32 \
+ember-minimal-printf \
+emdrv \
+emlib \
+external-generated-files \
+form-and-join \
+generic-interrupt-control/EFR32 \
+green-power-client \
+green-power-common \
+groups-server \
+hal-library/EFR32 \
+hal-library/EFR32_2 \
+heartbeat \
+ias-zone-server \
+identify \
+idle-sleep \
+mbedtls/EFR32 \
+network-steering \
+nvm3/EFR32 \
+nvm3 \
+on-off \
+packet-handoff \
+reporting \
+scan-dispatch \
+scenes \
+serial/EFR32 \
+serial \
+simple-main \
+strong-random \
+update-tc-link-key \
+zcl-framework-core \
+
